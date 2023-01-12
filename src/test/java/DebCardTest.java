e92e3b2a3d416d113dbd84b0403f12a5becc24b9
taggertt <taggertt@mail.ru>
2023-01-12 21:24:25 +0600

    rework chrome options

--- src/test/java/DebCardTest.java
+++ src/test/java/DebCardTest.java
@@ -29,13 +29,13 @@ public class DebCardTest {
     @BeforeEach
     void setUp() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.addArguments("--no-sandbox");
+        //options.addArguments("start-maximized"); // open Browser in maximized mode
+      //options.addArguments("disable-infobars"); // disabling infobars
+      //options.addArguments("--disable-extensions"); // disabling extensions
+      //options.addArguments("--disable-gpu"); // applicable to windows os only
+      //options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+      //options.addArguments("--no-sandbox"); // Bypass OS security model
+        //options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver();
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
